7b787262c67e83728f8c7ffe20a3228c35f1a0cc
Loren Segal <lsegal@soen.ca>
2009-12-24 19:09:13 -0500

    Add linkifying of constants/methods for syntax highlighted Ruby code blocks in 1.9

--- lib/yard/templates/helpers/html_syntax_highlight_helper.rb
+++ lib/yard/templates/helpers/html_syntax_highlight_helper.rb
@@ -2,16 +2,20 @@ module YARD
   module Templates
     module Helpers
       module HtmlSyntaxHighlightHelper
+        include ModuleHelper
+        
         def html_syntax_highlight_ruby(source)
           tokenlist = Parser::Ruby::RubyParser.parse(source, "(syntax_highlight)").tokens
           output = ""
-          tokenlist.each do |s|
+          tokenlist.each_with_index do |s, i|
             output << "<span class='tstring'>" if [:tstring_beg, :regexp_beg].include?(s[0])
             case s.first
             when :nl, :ignored_nl, :sp
               output << h(s.last)
-            when :ident
-              output << "<span class='id #{h(s.last)}'>#{h(s.last)}</span>"
+            when :ident, :const
+              klass = s.first == :ident ? "id #{h(s.last)}" : s.first
+              token = html_syntax_link_const(tokenlist, i)
+              output << "<span class='#{klass}'>#{token}</span>"
             else
               output << "<span class='#{s.first}'>#{h(s.last)}</span>"
             end
@@ -20,6 +24,40 @@ module YARD
           output
         rescue Parser::ParserSyntaxError
           h(source)
+        end
+        
+        private
+        
+        def html_syntax_link_const(tokenlist, index)
+          token = tokenlist[index]
+          if token.first == :const
+            text = h(group_const_path_ref(token.last, tokenlist, index + 1))
+          else
+            text = h(token.last)
+          end
+          if obj = Registry.resolve(object.namespace || :root, token.last)
+            if obj.is_a?(CodeObjects::MethodObject)
+              obj = prune_method_listing([obj], false).first
+            else
+              obj = run_verifier([obj]).first
+            end
+            if obj && obj != object
+              text = link_object(obj, text)
+            end
+          end
+          text
+        end
+        
+        def group_const_path_ref(token, tokenlist, start_index)
+          return token unless tokenlist[start_index] == [:op, '::']
+          token = token.dup
+          nxt = tokenlist[start_index]
+          while nxt && (nxt == [:op, '::'] || nxt.first == :const)
+            token << nxt.last
+            tokenlist.delete_at(start_index + 1)
+            nxt = tokenlist[start_index + 1]
+          end
+          token
         end
       end
     end
--- spec/templates/helpers/html_syntax_highlight_helper_spec.rb
+++ spec/templates/helpers/html_syntax_highlight_helper_spec.rb
@@ -4,6 +4,10 @@ describe YARD::Templates::Helpers::HtmlSyntaxHighlightHelper do
   include YARD::Templates::Helpers::HtmlHelper
   include YARD::Templates::Helpers::HtmlSyntaxHighlightHelper
   
+  before do
+    stub!(:object).and_return(CodeObjects::NamespaceObject.new(:root, :YARD))
+  end
+  
   describe '#html_syntax_highlight' do
     it "should not highlight source if options[:no_highlight] is set" do
       should_receive(:options).and_return(:no_highlight => true)
@@ -35,5 +39,15 @@ describe YARD::Templates::Helpers::HtmlSyntaxHighlightHelper do
       should_receive(:options).and_return(:no_highlight => false)
       html_syntax_highlight("def &x; ... end").should == "def &amp;x; ... end"
     end if RUBY19
+    
+    it "should link constants/methods (ruby19)" do
+      other = CodeObjects::NamespaceObject.new(:root, :Other)
+      should_receive(:run_verifier).with([other]).and_return([other])
+      should_receive(:link_object).with(other, "Other").and_return("LINK!")
+      result = html_syntax_highlight("def x; Other end")
+      html_equals_string(result, "<span class='kw'>def</span> 
+        <span class='id x'>x</span><span class='semicolon'>;</span> 
+        <span class='const'>LINK!</span> <span class='kw'>end</span>")
+    end
   end
 end
